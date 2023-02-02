b1fcd7b251fb6cf5811ebb20532c422635fdf6c4
s21014 <soccer0907>
2023-02-02 12:16:20 +0900

    選択した公園の名前と画像が出る

--- app/src/main/java/jp/ac/it_college/std/s21014/map/SubActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s21014/map/SubActivity.kt
@@ -1,8 +1,13 @@
 package jp.ac.it_college.std.s21014.map
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.squareup.picasso.Picasso
 import jp.ac.it_college.std.s21014.map.databinding.ActivitySubBinding
+import org.json.JSONArray
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class SubActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySubBinding
@@ -10,5 +15,32 @@ class SubActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySubBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val id = intent.getStringExtra("id") ?: "1"
+
+        onMarkerSet(id)
+    }
+
+    private fun onMarkerSet(id: String) {
+        val assetManager = resources.assets
+        val inputStream = assetManager.open("data.json")
+        val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+        val dataJson: String = bufferedReader.readText()
+        val rootJson = JSONArray(dataJson)
+
+        for (i in 0 until rootJson.length()) {
+            val data = rootJson.getJSONObject(i)
+            val currentId = data.getString("id")
+            if (currentId == id) {
+                binding.parkName.text = data.getString("park")
+                Picasso.get().load(data.getString("link"))
+                    .into(binding.parkImage)
+            }
+        }
+
+        binding.back.setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
