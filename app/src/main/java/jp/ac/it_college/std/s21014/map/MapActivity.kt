0d7f902ae510d4a03f00ddc8b582fb56165b35b4
s21014 <soccer0907>
2023-02-08 10:30:08 +0900

    現在地から周辺の公園を表示する

---
+++ app/src/main/java/jp/ac/it_college/std/s21014/map/MapActivity.kt
@@ -0,0 +1,431 @@
+package jp.ac.it_college.std.s21014.map
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.location.Location
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.TextView
+import androidx.annotation.UiThread
+import androidx.annotation.WorkerThread
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.os.HandlerCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
+import com.google.android.libraries.places.api.net.PlacesClient
+import org.json.JSONArray
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.maps.android.PolyUtil
+import jp.ac.it_college.std.s21014.map.BuildConfig.MAPS_API_KEY
+import jp.ac.it_college.std.s21014.map.databinding.ActivityMapBinding
+import org.json.JSONObject
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.URL
+import java.util.concurrent.Executors
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+
+    private var map: GoogleMap? = null
+    private lateinit var binding: ActivityMapBinding
+    private var cameraPosition: CameraPosition? = null
+    private lateinit var placesClient: PlacesClient
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val defaultLocation = LatLng(-33.8523341, 151.2106085)
+    private var locationPermissionGranted = false
+    private var lastKnownLocation: Location? = null
+    private var likelyPlaceNames: Array<String?> = arrayOfNulls(0)
+    private var likelyPlaceAddresses: Array<String?> = arrayOfNulls(0)
+    private var likelyPlaceAttributions: Array<List<*>?> = arrayOfNulls(0)
+    private var likelyPlaceLatLngs: Array<LatLng?> = arrayOfNulls(0)
+    private var mapLine: Polyline? = null
+    private val debugTag = "Sample"
+
+    companion object {
+        private val TAG = MapsActivity::class.java.simpleName
+        private const val DEFAULT_ZOOM = 15
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+        private const val KEY_CAMERA_POSITION = "camera_position"
+        private const val KEY_LOCATION = "location"
+        private const val M_MAX_ENTRIES = 5
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (savedInstanceState != null) {
+            lastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION)
+            cameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION)
+        }
+
+        binding = ActivityMapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.back.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        }
+
+        Places.initialize(applicationContext, MAPS_API_KEY)
+        placesClient = Places.createClient(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.Map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        map?.let { map ->
+            outState.putParcelable(KEY_CAMERA_POSITION, map.cameraPosition)
+            outState.putParcelable(KEY_LOCATION, lastKnownLocation)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.current_place_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.option_get_place) {
+            showCurrentPlace()
+        }
+        return true
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+
+    //ここから
+    @SuppressLint("PotentialBehaviorOverride")
+    override fun onMapReady(map: GoogleMap) {
+        this.map = map
+        // Jsonファイル読み込み
+        onMarkerSet()
+
+        this.map?.setInfoWindowAdapter(object : GoogleMap.InfoWindowAdapter {
+            override fun getInfoWindow(arg0: Marker): View? {
+                return null
+            }
+
+            override fun getInfoContents(marker: Marker): View {
+                val infoWindow = layoutInflater.inflate(R.layout.custom_info_contents,
+                    findViewById<FrameLayout>(R.id.Map), false)
+                val title = infoWindow.findViewById<TextView>(R.id.title)
+                title.text = marker.title
+                val snippet = infoWindow.findViewById<TextView>(R.id.snippet)
+                snippet.text = marker.snippet
+                return infoWindow
+            }
+        })
+        map.uiSettings.isZoomControlsEnabled = true
+
+        map.setOnMapClickListener {
+            mapLine?.isVisible = false
+        }
+        getLocationPermission()
+        updateLocationUI()
+        getDeviceLocation()
+    }
+
+    @SuppressLint("PotentialBehaviorOverride")
+    private fun onMarkerSet() {
+        val assetManager = resources.assets
+        val inputStream = assetManager.open("data.json")
+        val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+        val dataJson: String = bufferedReader.readText()
+        // Jsonファイルのセット
+        val rootJson = JSONArray(dataJson)
+
+        // マーカー表示
+        for (i in 0 until rootJson.length()) {
+            val data = rootJson.getJSONObject(i)
+            val id = data.getString("id")
+            val parkName = data.getString("park")
+            val address = data.getString("address")
+            val lat = data.getDouble("latitude")
+            val lon = data.getDouble("longitude")
+            val park = LatLng(lat, lon)
+
+            map?.addMarker(
+                MarkerOptions()
+                    .position(park)
+                    .title(parkName)
+                    .snippet(address)
+                    .icon(BitmapDescriptorFactory.fromResource(R.mipmap.park))
+            )?.tag = id
+            map?.moveCamera(CameraUpdateFactory.newLatLng(park))
+        }
+
+        map?.setOnMarkerClickListener(this)
+
+        map?.setOnInfoWindowClickListener { marker ->
+            val id = marker.tag.toString()
+            val intent = Intent(this@MapActivity, SubActivity::class.java).apply {
+                putExtra("id", id)
+            }
+            startActivity(intent)
+            return@setOnInfoWindowClickListener
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onMarkerClick(marker: Marker): Boolean {
+        // ルートの削除
+        mapLine?.isVisible = false
+
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            location?.let {
+                val start = "${it.latitude},${it.longitude}"
+                val dest = "${marker.position.latitude},${marker.position.longitude}"
+                getRoutes(start, dest)
+            }
+        }
+        return false
+    }
+
+    @UiThread
+    private fun getRoutes(start: String, dest: String) {
+        val handler = HandlerCompat.createAsync(mainLooper)
+        val executeService = Executors.newSingleThreadExecutor()
+
+        executeService.submit @WorkerThread {
+            val urlHeader = "https://maps.googleapis.com/maps/api/directions/json?"
+            val origin = "origin=$start&"
+            val destination = "destination=$dest&"
+            val key = "key=$MAPS_API_KEY"
+            val url = URL(urlHeader + origin + destination + key)
+            val con = url.openConnection() as? HttpURLConnection
+            var result = ""
+            con?.let {
+                try {
+                    it.connectTimeout = 10000
+                    it.readTimeout = 10000
+                    it.requestMethod = "GET"
+                    it.connect()
+
+                    val stream = it.inputStream
+                    result = is2String(stream)
+                    stream.close()
+                } catch (e: SocketTimeoutException) {
+                    Log.w(debugTag, "通信タイムアウト", e)
+                }
+                it.disconnect()
+            }
+            handler.post @UiThread {
+                val root = JSONObject(result)
+                val routes = root.getJSONArray("routes")
+                val getArray = routes.getJSONObject(0)
+                val overviewPolyline = getArray.getJSONObject("overview_polyline")
+                val points = overviewPolyline.getString("points")
+                val p = PolyUtil.decode(points)
+
+                if (map?.addPolyline(PolylineOptions())!!.isVisible) {
+                    map?.addPolyline(PolylineOptions())!!.isVisible = false
+                }
+
+                mapLine = map?.addPolyline(
+                    PolylineOptions()
+                        .clickable(true)
+                        .color(Color.BLUE)
+                        .width(9f)
+                        .addAll(p)
+                )
+            }
+        }
+    }
+
+    private fun is2String(stream: InputStream?): String {
+        val reader = BufferedReader(InputStreamReader(stream, "UTF-8"))
+        return reader.readText()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getDeviceLocation() {
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationClient.lastLocation
+                locationResult.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        lastKnownLocation = task.result
+                        if (lastKnownLocation != null) {
+                            map?.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                LatLng(lastKnownLocation!!.latitude,
+                                    lastKnownLocation!!.longitude), DEFAULT_ZOOM.toFloat()))
+                        }
+                    } else {
+                        Log.d(TAG, "Current location is null. Using defaults.")
+                        Log.e(TAG, "Exception: %s", task.exception)
+                        map?.moveCamera(CameraUpdateFactory
+                            .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat()))
+                        map?.uiSettings?.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+    private fun getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this.applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        locationPermissionGranted = false
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true
+                }
+            }
+        }
+        updateLocationUI()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun showCurrentPlace() {
+        if (map == null) {
+            return
+        }
+        if (locationPermissionGranted) {
+            val placeFields = listOf(Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG)
+            val request = FindCurrentPlaceRequest.newInstance(placeFields)
+            val placeResult = placesClient.findCurrentPlace(request)
+            placeResult.addOnCompleteListener { task ->
+                if (task.isSuccessful && task.result != null) {
+                    val likelyPlaces = task.result
+                    val count =
+                        if (likelyPlaces != null && likelyPlaces.placeLikelihoods.size < M_MAX_ENTRIES) {
+                            likelyPlaces.placeLikelihoods.size
+                        } else {
+                            M_MAX_ENTRIES
+                        }
+                    var i = 0
+                    likelyPlaceNames = arrayOfNulls(count)
+                    likelyPlaceAddresses = arrayOfNulls(count)
+                    likelyPlaceAttributions = arrayOfNulls<List<*>?>(count)
+                    likelyPlaceLatLngs = arrayOfNulls(count)
+                    for (placeLikelihood in likelyPlaces?.placeLikelihoods ?: emptyList()) {
+                        likelyPlaceNames[i] = placeLikelihood.place.name
+                        likelyPlaceAddresses[i] = placeLikelihood.place.address
+                        likelyPlaceAttributions[i] = placeLikelihood.place.attributions
+                        likelyPlaceLatLngs[i] = placeLikelihood.place.latLng
+                        i++
+                        if (i > count - 1) {
+                            break
+                        }
+                    }
+                    openPlacesDialog()
+                } else {
+                    Log.e(TAG, "Exception: %s", task.exception)
+                }
+            }
+        } else {
+            Log.i(TAG, "The user did not grant location permission.")
+            map?.addMarker(MarkerOptions()
+                .title(getString(R.string.default_info_title))
+                .position(defaultLocation)
+                .snippet(getString(R.string.default_info_snippet))
+            )
+            getLocationPermission()
+        }
+    }
+
+    private fun openPlacesDialog() {
+        val listener = DialogInterface.OnClickListener { _, which ->
+            val markerLatLng = likelyPlaceLatLngs[which]
+            var markerSnippet = likelyPlaceAddresses[which]
+            if (likelyPlaceAttributions[which] != null) {
+                markerSnippet = """
+                    $markerSnippet
+                    ${likelyPlaceAttributions[which]}
+                """.trimIndent()
+            }
+
+            if (markerLatLng == null) {
+                return@OnClickListener
+            }
+
+            map?.addMarker(
+                MarkerOptions()
+                    .title(likelyPlaceNames[which])
+                    .position(markerLatLng)
+                    .snippet(markerSnippet)
+            )
+
+            map?.moveCamera(CameraUpdateFactory.newLatLngZoom(markerLatLng, DEFAULT_ZOOM.toFloat()))
+        }
+        AlertDialog.Builder(this)
+            .setTitle(R.string.pick_place)
+            .setItems(likelyPlaceNames, listener)
+            .show()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun updateLocationUI() {
+        if (map == null) {
+            return
+        }
+        try {
+            if (locationPermissionGranted) {
+                map?.isMyLocationEnabled = true
+                map?.uiSettings?.isMyLocationButtonEnabled = true
+            } else {
+                map?.isMyLocationEnabled = false
+                map?.uiSettings?.isMyLocationButtonEnabled = false
+                lastKnownLocation = null
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+}
